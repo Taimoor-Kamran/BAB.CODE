e830f0a0439ec97d5073e136de663d049fc5a85c
Taimoor-Kamran <muhammadtaimoor2006@gmail.com>
2025-08-08 21:01:32 +0500

    added 3 commit

--- vite-project/vite.config.ts
+++ vite-project/vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
+  base: "./", // ✅ This ensures correct asset resolution on Vercel
+  plugins: [tailwindcss()],
 })
